8e185100ab1fd78f7029c94a4b2f0fc22fd13e57
Akash <ak.parmar1442000@gmail.com>
2025-11-13 11:08:01 +0530

    Update FirstSection component for improved layout and responsiveness
    - Centered background image for better visual alignment.
    - Adjusted heading sizes for improved readability across devices.
    - Enhanced stats section layout for better spacing and alignment.
    - Updated image handling for better responsiveness.

--- src/components/website/landing-page/FirstSection.tsx
+++ src/components/website/landing-page/FirstSection.tsx
@@ -29,34 +29,36 @@ function FirstSection() {
       <div
         className="flex flex-col items-center bg-cover bg-center px-6 relative"
       >
-        <div className="absolute -top-10 left-0 h-[539px] w-[950px] bg-[url('/website/hero-bg.png')] bg-no-repeat bg-center bg-cover z-0" />
+        <div className="absolute -top-10 left-1/2 -translate-x-1/2 h-[539px] w-[950px] bg-[url('/website/hero-bg.png')] bg-no-repeat bg-center bg-cover z-0" />
         {/* Hero Text */}
         <div className="text-center relative z-10">
           <h1
-            className="text-6xl 2xl:text-7xl font-semibold leading-none gradient-text"
+            className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl font-semibold leading-normal gradient-text"
           >
             Transparency, Verified Ratings,
           </h1>
           <h1
-            className="text-6xl 2xl:text-7xl font-semibold leading-none gradient-text mt-2"
+            className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl font-semibold leading-none gradient-text mt-2"
           >
             Traders Trust.
           </h1>
         </div>
 
         {/* Stats Section */}
-        <div className="flex justify-between w-full mt-14 text-white relative z-10">
+        <div className="max-w-7xl mx-auto flex flex-col  md:flex-row justify-between w-full mt-8 md:mt-10 xl:mt-14 text-white relative z-10">
           {stats.map((item, index) => (
             <div
               key={index}
               className="flex items-center gap-3 justify-center  py-2  "
             >
-              <Image src={item.src} alt="verify-badge" height={28} width={28} />
+              <div className=" w-6 h-6 2xl:w-8 2xl:h-8 rounded flex flex-col relative">
+                <Image src={item.src} alt="verify-badge" fill />
+              </div>
               <div className="flex gap-2 items-center text-white/60">
-                <p className="font-bold text-[20px] leading-[100%]">
+                <p className="font-bold text-base md:text-[20px] leading-[100%]">
                   {item.value}
                 </p>
-                <p className="text-[20px] font-light tracking-[-0.05em]">
+                <p className="text-base md:text-[20px] font-light tracking-[-0.05em]">
                   {item.label}
                 </p>
               </div>
