e7ebfd0b0a62185f229b4e9f1b3f8986a2c76c13
Akash <ak.parmar1442000@gmail.com>
2025-12-09 12:39:23 +0530

    feat: Add LoadingPage component for improved user experience
    - Introduced LoadingPage component that displays a loading screen with a title and subtitle.
    - Utilized LoadingScreen component for consistent UI during loading states.

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingScreen from '@/components/ui/LoadingScreen'
+import React from 'react'
+
+const LoadingPage = () => {
+  return (
+    <LoadingScreen title="Getting things ready..." subtitle="This will only take a moment." />
+  )
+}
+
+export default LoadingPage
